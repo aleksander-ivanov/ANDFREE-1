fdc665f87429049cdfa7a767dd3455d59e488622
Alex Ivanov <ivanov_alex_85@mail.ru>
2021-02-24 23:10:42 +0400

    Домашнее задание к занятию 4. Добавление голосового интерфейса

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -1,16 +1,28 @@
 package com.example.myapp
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.RecognizerIntent
+import android.speech.tts.TextToSpeech
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.wolfram.alpha.WAEngine
 import com.wolfram.alpha.WAPlainText
+import org.w3c.dom.Text
+import java.util.*
+import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
 
     private val MY_APP: String = "MyApp"
+    private val speechToTextRequestCode = 123
+    lateinit var textToSpeech: TextToSpeech
+    var speechRequest = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         Log.d(MY_APP, "starting...")
@@ -23,13 +35,46 @@ class MainActivity : AppCompatActivity() {
         val searchButton = findViewById<Button>(R.id.search_button)
 
         searchButton.setOnClickListener {
+            textToSpeech.stop()
             try {
                 askWolfram(questionInput.text.toString())
             }
             catch (e: Exception){
                 Log.e("wolfram error", e.message.toString())
-
             }
+        }
+
+        val speakButton = findViewById<Button>(R.id.speak_button)
+        speakButton.setOnClickListener{
+            textToSpeech.stop()
+            
+            val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+            intent.putExtra(
+                RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+            )
+            intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "What would you like to know?")
+
+            try {
+                startActivityForResult(intent, speechToTextRequestCode)
+            }catch (e: ActivityNotFoundException){
+                Log.e("wolfram error", e.message.toString())
+                Toast.makeText(
+                    applicationContext,
+                    "Sorry, your device is not supported",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+
+        val answerOutput = findViewById<TextView>(R.id.answer_output)
+        textToSpeech = TextToSpeech(this, TextToSpeech.OnInitListener {  })
+        textToSpeech.language = Locale.US
+
+        findViewById<FloatingActionButton>(R.id.read_answer).setOnClickListener{
+            val answer = answerOutput.text.toString()
+            textToSpeech.speak(answer, TextToSpeech.QUEUE_ADD, null, speechRequest.toString())
+            speechRequest +=1
         }
 
         Log.d(MY_APP, "End of Create function")
@@ -76,6 +121,25 @@ class MainActivity : AppCompatActivity() {
             answerText.post {
                 answerText.text = answer
             }
+            //val view = findViewById<TextView>(R.id.answer_output).text.toString()
+            textToSpeech.speak(answer, TextToSpeech.QUEUE_ADD, null, speechRequest.toString())
         }).start()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == speechToTextRequestCode){
+            if(resultCode == RESULT_OK && data != null){
+                val result: ArrayList<String>? = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+                val question: String? = result?.get(0)
+
+                if(!question.isNullOrEmpty()){
+                    findViewById<TextView>(R.id.question_input).text = question
+
+                    askWolfram(question)
+                }
+            }
+        }
     }
 }
